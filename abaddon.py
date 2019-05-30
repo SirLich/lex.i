625d83026ab2e48efd523c1d40ff4462f4b11fe4
SirLich <lrkoehler77@gmail.com>
2019-05-29 21:16:49 -0500

    Fixed variable name misspelling

--- abaddon.py
+++ abaddon.py
@@ -1,23 +1,30 @@
+#! /usr/bin/python3
+
 import discord
-from discord.ext import commands
-import random
-import asyncio
+import subprocess
+from subprocess import check_output
+import time
+client = discord.Client()
 
-command_prefix='!'
-bot = commands.Bot(command_prefix)
+def scramble(sentence):
+    out = check_output(["python","bristol/main.py",sentence])
+    print(out)
+    return out
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Connected!')
-        print('Username: {0.name}\nID: {0.id}'.format(self.user))
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
-    @bot.command(description='For when you wanna settle the score some other way')
-    async def choose(ctx, *choices: str):
-        """Chooses between multiple choices."""
-        await ctx.send(random.choice(choices))
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
+    if message.guild.me in message.mentions:
+        sentence = message.content.replace("<@418271589356797952>","")
+        sentence = str(scramble(sentence),'utf-8')
+        await message.channel.send(sentence)
 
-client = MyClient()
 f = open("token.txt","r")
 token = f.read().strip()
 client.run(token)
