ea6ad0e06cb9d6eca5bea9e96e1e3a60b00408f7
SirLich <lrkoehler77@gmail.com>
2022-07-01 22:35:16 +0200

    Unknown push

--- bristol/main.py
+++ bristol/main.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python
-
 import requests
 import json
 import re
-import en
+#import en
 import random
 import sys
 
@@ -18,40 +16,14 @@ def get_api_key():
     return key
 
 def build_url(word):
-    url = 'http://words.bighugelabs.com/api/2/{}/{}/'.format(get_api_key(),word)
+    url = 'http://words.bighugelabs.com/api/2/{}/{}/'.format(get_api_key(), word)
     return url
 
 def strip_to_alpha(word):
     return re.sub(r'\W+', '', word).lower()
 
-#Credit: https://gist.github.com/hugsy/8910dc78d208e40de42deb29e62df913
-def is_adjective(word):
-    word = " " + word + " "
-    if word in open('adjectives.txt').read():
-        return True
-    return False
-
-def process_touple(tup):
-    word = tup[0]
-    part = tup[1]
-    if(part in (".",",",":")):
-        return word
-    if(part in ("VB","VBD","VBG","VBP","VBZ")):
-        return " " + fetch_synonym(word,part)
-
-    if(part in ("NN","NNP","NNPS","NNS","NP","JJ")):
-        return " " + fetch_synonym(word,part)
-    return " " + word
-
 def fetch_synonym(word,part):
     out = word
-
-    # #Verb specific
-    # if(part in ("VB","VBD","VBG","VBP","VBZ")):
-    #     tense = en.verb.tense(word)
-    #     word = en.verb.infinitive(word)
-    #     if(word == "be"):
-    #         return out
 
     URL = build_url(word)
     response = requests.get(url = URL)
@@ -66,17 +38,8 @@ def fetch_synonym(word,part):
     if(len(synonyms) > 0):
         out = random.choice(synonyms)
 
-    # #Verb specific
-    # if(part in ("VB","VBD","VBG","VBP","VBZ") and tense):
-    #     out = en.verb.conjugate(out,tense)
-
     return out
 
-def convert_semantic_marker_to_english(sem):
-    if(sem in ("VB","VBD","VBP","VBZ")):
-        return "verb"
-    else:
-        return "noun"
 
 def process_sentance(sentance):
     out = ""
@@ -86,18 +49,13 @@ def process_sentance(sentance):
         out += process_touple(tup)
     return out
 
-
-def touple_print(semantic):
-    for tup in semantic:
-        print(tup[0] + " " + tup[1])
-
 def test_run():
     while(True):
-        sentance = raw_input("Enter some text: ")
+        sentance = input("Enter some text: ")
         print(process_sentance(sentance))
 
 def main(args):
     sentance = args[1]
-    print process_sentance(sentance)
+    print(process_sentance(sentance))
 
 main(sys.argv)
